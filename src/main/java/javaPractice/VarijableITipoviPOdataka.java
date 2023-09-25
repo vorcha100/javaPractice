f7385d2894e2531858b7daaf621afc6ae785b755
vorcha100 <85402918+vorcha100@users.noreply.github.com>
2023-09-25 09:57:15 +0200

    addedContent

--- src/main/java/javaPractice/VarijableITipoviPOdataka.java
+++ src/main/java/javaPractice/VarijableITipoviPOdataka.java
@@ -2,6 +2,21 @@ package javaPractice;
 
 public class VarijableITipoviPOdataka {
 
+    public static void main (String[]args) {
 
+        String ime = "Sloba";
+        int godine = 40;
+        double visina = 1.82;
+        boolean zaposlen = true;
+        float plata = 500.5f;
+        char pol = 'M';
+
+        System.out.println("Moje ime je " + ime + ".");
+        System.out.println("Imam " + godine + " godina.");
+        System.out.println("Visok sam " + visina + " metara.");
+        System.out.println("Zaposlen sam " + zaposlen + " u kompaniji HTEC.");
+        System.out.println("Moja plate je " + plata + " evra.");
+        System.out.println("Ja sam " + pol + ".");
+    }
 
 }
